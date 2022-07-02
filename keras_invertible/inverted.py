2f4815d71928ddf4c4b306e1ca1edafc1005143d
Lance Norskog <LanceNorskog@users.noreply.github.com>
2022-07-01 21:27:39 -0700

    Update inverted.py

--- keras_invertible/inverted.py
+++ keras_invertible/inverted.py
@@ -35,8 +35,8 @@ class InvertedBias(tf.keras.layers.Layer):
 
 """ Force layer to remain the Penrose pseudo-inverse of the master layer """
 class InvertedDensePI(tf.keras.layers.Layer):
-     def __init__(self, master_layer):
-        super(InvertedDensePI, self).__init__()
+     def __init__(self, master_layer, **kwargs):
+        super(InvertedDensePI, self).__init__(**kwargs)
         self.master_layer = master_layer
 
     def build(self, input_shape):
