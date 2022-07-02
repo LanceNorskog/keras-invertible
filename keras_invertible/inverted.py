ec0c6108e9d3fdea7c54408cc371bb5b157721d6
Lance Norskog <LanceNorskog@users.noreply.github.com>
2022-07-01 19:44:48 -0700

    Add pseudo-inverse version of InvertedDense

--- keras_invertible/inverted.py
+++ keras_invertible/inverted.py
@@ -22,3 +22,24 @@ class InvertedDense(layers.Layer):
         b = self.master_layer._trainable_weights[1]
         w = tf.transpose(W)
         return tf.matmul(inputs - b, w)
+
+    """ Given a master layer, invert bias then transpose weights """
+class InvertedDensePI(layers.Layer):
+    def __init__(self, master_layer):
+        super(TiedDense, self).__init__()
+        self.master_layer = master_layer
+
+    def build(self, input_shape):
+        # do not train weights or bias from master_layer, they are read-only
+        self.params = []
+        
+    def call(self, inputs):  # Defines the computation from inputs to outputs
+        (W, b) = self.master_layer.get_weights()
+        print(type(W))
+        print('W.shape', W.shape)
+        print('b.shape', b.shape)
+        PI = np.linalg.pinv(W)
+#         W = self.master_layer._trainable_weights[0]
+#         b = self.master_layer._trainable_weights[1]
+#         w = tf.transpose(W)
+        return tf.matmul(inputs - b, w)
