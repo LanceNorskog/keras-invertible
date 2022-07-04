16383f6f25ad7815fafcca7ce637ab17c31722fc
Lance Norskog <LanceNorskog@users.noreply.github.com>
2022-07-04 00:20:58 -0700

    Update inverted.py

--- keras_invertible/inverted.py
+++ keras_invertible/inverted.py
@@ -98,3 +98,45 @@ class InvertedLeakyReLU(tf.keras.layers.Layer):
         config = {"alpha": float(self.alpha)}
         base_config = super().get_config()
         return dict(list(base_config.items()) + list(config.items()))
+
+class MonotonicPReLU(Layer):
+    """Monotonic Parametric Rectified Linear Unit.
+    It follows:
+    ```
+      f(x) = alpha * x for x < 0
+      f(x) = x for x >= 0
+    ```
+    where `alpha` is a learned array with the same shape as x.
+    To achieve monotonicity, force learned alpha params above 0 via abs().
+    Input shape:
+      Arbitrary. Use the keyword argument `input_shape`
+      (tuple of integers, does not include the samples axis)
+      when using this layer as the first layer in a model.
+    Output shape:
+      Same shape as the input.
+    """
+
+    def __init__(
+        self,
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.supports_masking = True
+
+    @tf_utils.shape_type_conversion
+    def build(self, input_shape):
+        param_shape = list(input_shape[1:])
+        self.alpha = self.add_weight(
+            shape=param_shape,
+            name="alpha",
+        )
+        self.input_spec = InputSpec(ndim=len(input_shape), axes={})
+        self.built = True
+
+    def call(self, inputs):
+        pos = backend.relu(inputs)
+        neg = -tf.math.abs(self.alpha) * backend.relu(-inputs)
+        return pos + neg
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
